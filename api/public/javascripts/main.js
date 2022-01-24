26af20bf9dfd35b4d900366ad0d37ca0983c4a57
sakerlucas <sakerlucas@gmail.com>
2022-01-24 18:13:41 +0100

    debut des fonctions et recuperation du mot

---
+++ api/public/javascripts/main.js
@@ -0,0 +1,26 @@
+let langue
+let mot
+//let url = `https://api.dictionaryapi.dev/api/v2/entries/`
+
+function definition(l, m) {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/${l}/${m}`)
+        .then(response => response.json())
+        .then((results) => {
+            console.log(results);
+            document.getElementById('def').innerHTML += `
+                <h1>${results[0].word}</h1>
+            `
+        })
+        .catch(err => console.log(err))
+}
+
+window.onload = () => {
+    document.getElementById('bouton').addEventListener('click', (evt) => {
+        evt.preventDefault()
+        console.log('click');
+        mot = document.getElementById('saisie').value
+        langue = document.getElementById('langue').value
+
+        definition(langue, mot)
+    })
+}
