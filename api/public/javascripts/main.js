3460019e6e8e881577311af91bb4a110580552b2
sakerlucas <sakerlucas@gmail.com>
2022-01-29 12:14:44 +0100

    add loader

--- api/public/javascripts/main.js
+++ api/public/javascripts/main.js
@@ -4,40 +4,43 @@ window.onload = () => {
     document.getElementById('bouton').addEventListener('click', (evt) => {
         evt.preventDefault()
         console.log('Recherche');
+        document.getElementById('def').innerHTML = `
+            <div class="loader m-auto mt-3" id="loader">
+            </div>
+        `
         mot = document.getElementById('saisie').value
         langue = document.getElementById('langue').value
 
         fetch(`https://api.dictionaryapi.dev/api/v2/entries/${langue}/${mot}`)
-        .then(response => response.json())
-        .then((results) => {
-            
-            document.getElementById('def').innerHTML = `
+            .then(response => response.json())
+            .then((results) => {
+
+                document.getElementById('def').innerHTML = `
                 <h1>${results[0].word}</h1>
             `
 
-            if (langue != 'en') {
-                document.getElementById('def').innerHTML += `
+                if (langue != 'en') {
+                    document.getElementById('def').innerHTML += `
                     <small>(${results[0].meanings[0].partOfSpeech})</small>
                     <ul>
                 `
-            } else {
-                document.getElementById('def').innerHTML += `
+                } else {
+                    document.getElementById('def').innerHTML += `
                     <ul>
                 `
-            }
+                }
 
-            for (let i = 0; i < results[0].meanings[0].definitions.length; i++) {
-                document.getElementById('def').innerHTML += `
+                for (let i = 0; i < results[0].meanings[0].definitions.length; i++) {
+                    document.getElementById('def').innerHTML += `
                     <li>${results[0].meanings[0].definitions[i].definition}</li>
                 `
-                
-            document.getElementById('def').innerHTML += `
+
+                    document.getElementById('def').innerHTML += `
                 </ul>
             `
-            }
-            
-        })
-        .catch(err => console.log(err))
+                }
+            })
+            .catch(err => console.log(err))
     })
 
     document.getElementById('boutonDelete').addEventListener('click', (evt) => {
